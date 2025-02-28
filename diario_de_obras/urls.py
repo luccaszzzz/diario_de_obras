f69160d3de948831e625d1f74fa34f7154f65b56
Lucas Emanoel da Silva Freitas <20231198060018@ifrn.local>
2025-02-28 16:15:23 -0300

    ajuste no front

--- diario_de_obras/urls.py
+++ diario_de_obras/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import login
+from core.views import home, login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', login, name='login'),
+    path('home/', home, name='home'),
 ]
